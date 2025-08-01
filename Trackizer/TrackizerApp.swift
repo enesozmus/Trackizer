4e65826a5964a74c80207cc4acc465209170a4a1
enesozmus <ensozmus@gmail.com>
2025-08-01 14:35:39 +0300

    Initial Commit

---
+++ Trackizer/TrackizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackizerApp.swift
+//  Trackizer
+//
+//  Created by enesozmus on 1.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrackizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
