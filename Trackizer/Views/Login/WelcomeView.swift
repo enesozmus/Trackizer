d84e015b73354923d61a214331944f840fd5080c
enesozmus <ensozmus@gmail.com>
2025-08-08 12:28:24 +0300

    Welcome UI Screen

---
+++ Trackizer/Views/Login/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Trackizer
+//
+//  Created by enesozmus on 8.08.2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
